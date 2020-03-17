49a47322a7eb529d4105251be309339d83de0dae
Milica Ljubisavljevic <milic@DTC-76>
2020-03-17 08:48:38 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import MediaCard from './Profile';
 import SubmitPage from './SubmitPage';
-
+import Menu from './Menu';
 
 
 function App() {
   return (
     
     <div className="App">
-      <SubmitPage/>
+      <MediaCard/>
       
       
     </div>
