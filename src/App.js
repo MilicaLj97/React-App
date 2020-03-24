8c8d8d7117eac388968ea02da91fd2ecdbbc0e79
Milica Ljubisavljevic <milic@DTC-76>
2020-03-24 12:27:20 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,16 @@ import Menu from './Menu';
 import SubmitPage from './SubmitPage';
 import UserPage from './UserPage';
 import RoleList from './RoleList';
-<<<<<<< HEAD
-import Profile from './Profile';
-=======
 import CreateRole from './CreateRole';
->>>>>>> 0dd532c204dcc6aa667b65b2da87132541a1621f
+import Profile from './Profile';
 
 function App() {
   return (
     
     <div className="App">
-      
-<<<<<<< HEAD
-      <Profile/>
-      
-=======
+
       <RoleList/>
 
->>>>>>> 0dd532c204dcc6aa667b65b2da87132541a1621f
     </div>
     
   );
