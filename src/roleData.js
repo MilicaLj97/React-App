5bf2fa43695ebc084d667285c25939f9d9b8d808
Milica Ljubisavljevic <milic@DTC-76>
2020-03-24 10:07:16 +0100

    Added roleData for RoleList page

---
+++ src/roleData.js
@@ -0,0 +1,45 @@
+
+import Slika from './34190918.jpg';
+import logo from './dtc.jpg';
+
+const roleData = [
+    {
+      id: 1,
+      Name: 'Name 1',
+      AvailabilityFrom: '20.03.2020.',
+      AvailabilityTo: '21.07.2020.',
+      details: 'Detalji test test test test test test test test test test test test test test test test test test test',
+      logo: logo,
+  
+    },
+    {
+      id: 2,
+      Name: 'Name 2',
+      AvailabilityFrom: '20.03.2020.',
+      AvailabilityTo: '21.07.2020.',
+      details: 'Detalji test test test test test test test test test test test test test test test test test test test',
+      
+      
+    },
+   {
+    id: 3,
+      Name: 'Name 3',
+      AvailabilityFrom: '20.03.2020.',
+      AvailabilityTo: '21.07.2020.',
+      details: 'Detalji test test test test test test test test test test test test test test test test test test test',
+      
+      
+    },
+    {
+      id: 4,
+      Name: 'Name 4',
+      AvailabilityFrom: '20.03.2020.',
+      AvailabilityTo: '21.07.2020.',
+      details: 'Detalji test test test test test test test test test test test test test test test test test test test',
+      
+    },
+    
+    
+  ];
+  
+  export default roleData;
