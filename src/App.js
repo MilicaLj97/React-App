298b62a10529cb73e7f92a3e7572ad0a3fb27a58
Milica Ljubisavljevic <milic@DTC-76>
2020-03-24 09:23:21 +0100

    RoleList page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Menu from './Menu';
 import SubmitPage from './SubmitPage';
 import UserPage from './UserPage';
+import RoleList from './RoleList';
 
 function App() {
   return (
     
     <div className="App">
       
-      <Menu/>
+      <RoleList/>
       
     </div>
     
---
+++ src/RoleList.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import cardData from './cardData';
+import EditRoundedIcon from '@material-ui/icons/EditRounded';
+import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
+import AppBar from './AppBar';
+import Footer from './Footer';
+import PageShowing from './PageShowing';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex-inline',
+    float: 'left',
+    padding: 10,
+    
+  },
+  superdiv: {
+    display: 'flex',
+    width: 'auto',
+    height: 155,
+    boxShadow:'8px 8px 5px lightgrey', 
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+    
+  },
+  content: {
+    flex: '1 0 auto',
+    width: 500,
+  },
+  cover: {
+    width: 151,
+  },
+  controls: {
+
+    display: 'flex',
+    justifyContent: 'flex-end',
+    paddingLeft: theme.spacing(1),
+    //paddingBottom: theme.spacing(1),
+    height:'50px!important',
+    marginTop: 'auto',
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  },
+})); 
+
+ 
+export default function TitlebarGridList() {
+  const classes = useStyles();
+
+  return (
+   
+<div>
+  <AppBar/>
+{cardData.map(tile => (
+
+    <div className={classes.root}>
+      <div className={classes.superdiv}>
+      
+    <div className={classes.details}>
+
+      <CardContent className={classes.content}>
+        <Typography component="h5" variant="h5">
+          {tile.title}
+        </Typography>
+        <Typography variant="subtitle1" color="textSecondary">
+          {tile.status}
+        </Typography>
+        <Typography variant="subtitle1" >
+          {tile.details}
+        </Typography>
+      </CardContent>
+      </div>
+      <div className={classes.controls}>
+        <IconButton aria-label="previous" >
+          <EditRoundedIcon className={classes.icon}/>
+        </IconButton>
+        <IconButton aria-label="play/pause">
+          <DeleteRoundedIcon className={classes.icon}/>
+        </IconButton>
+        
+      </div>
+    
+    </div>
+  </div>
+  ))}
+  <PageShowing/>
+  <Footer/>
+</div>
+  );
+}
